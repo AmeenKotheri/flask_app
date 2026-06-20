22aaba2ffc58ede21be57a5c0f7a2d8829bb5f9c
AmeenKotheri <ameenkotheri@gmail.com>
2026-06-20 15:33:00 +0530

    Revert to Flask-Mail

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"]
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://"  # <-- Add this line
 )
 
 # CONFIG
